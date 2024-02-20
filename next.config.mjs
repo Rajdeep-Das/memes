9a36f26f5a419917ad01603de3c8538ca9d8e25c
Rajdeep-Das <rajdeepdas.india@gmail.com>
2024-02-21 03:13:40 +0530

    Base MVP With Static Data

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'rajdeep-das.s3.ap-south-1.amazonaws.com',
+                port: '',
+                pathname: '/project-memes/**',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
